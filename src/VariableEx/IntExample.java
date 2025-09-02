28a644b820831e24e8f5266d5b16b54805af6cd8
Chosunin <choinsun@g.ac.kr>
2025-09-02 19:24:12 +0900

    정수 기입 및 합 계산

--- src/VariableEx/IntExample.java
+++ src/VariableEx/IntExample.java
@@ -2,6 +2,9 @@ package VariableEx;
 // 두 개의 정수형 변수를 선언하고 초기화한 후, 두 숫자의 합을 계산하여 출력하세요.
 public class IntExample {
     public static void main(String[] args) {
+    int a = 4;
+    int b = 5;
 
+    System.out.println(a + b );
     }
 }
